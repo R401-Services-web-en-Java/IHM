7a404520c10e9320d22bc41ee13b16f284a6283f
18Nilsou <nils.saadi@etu.univ-amu.fr>
2023-03-24 19:47:59 +0100

    modification dans le header (session)

--- Views/standard/header.php
+++ Views/standard/header.php
@@ -1,3 +1,12 @@
 <header>
-    <h1> Mon Panier </h1> <a href = "/Signin"> Connexion </a>
+    <h1> Mon Panier </h1> 
+<?php if(Session::getSession()){
+    echo '<a href ="#"> Mon Compte </a>';
+    if(Session::getSession()['status'] == "admin"){
+        echo '<a href ="#"> Espace Administrateur </a>';
+    }
+}else{
+    echo '<a href ="/Signin"> Connexion </a>';
+}
+?>
 </header>
