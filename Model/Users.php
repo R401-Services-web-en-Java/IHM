c60d07dd4edf5d8e403e36c02fb15d84fecfc737
18Nilsou <nils.saadi@etu.univ-amu.fr>
2023-03-24 19:32:56 +0100

    ajout des signature de fonctions pour se connecter

---
+++ Model/Users.php
@@ -0,0 +1,15 @@
+<?php
+
+final class Users{
+
+    public static function isUser ($A_param){
+        #Test si il est dans l'api Users
+        return true;
+    }
+
+    public static function getStatus ($A_param){
+        #Test si il est admin dans l'api Users
+        return true;
+    }
+
+}
